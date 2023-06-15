caeea577ecbec3f1e0081f1fdb2034936c171940
omerbenda <omerbenda@gmail.com>
2023-06-15 18:02:02 +0300

    feat(ModItems.java, PotionBlocks.java): Add classes for registering new blocks

--- src/main/java/com/potionblocks/PotionBlocks.java
+++ src/main/java/com/potionblocks/PotionBlocks.java
@@ -1,8 +1,11 @@
 package com.potionblocks;
 
 import com.mojang.logging.LogUtils;
+import com.potionblocks.block.ModBlocks;
+import com.potionblocks.item.ModItems;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,12 +22,16 @@ public class PotionBlocks {
   public PotionBlocks() {
     IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+    ModItems.register(modEventBus);
+
     modEventBus.addListener(this::commonSetup);
 
     MinecraftForge.EVENT_BUS.register(this);
   }
 
   private void commonSetup(final FMLCommonSetupEvent event) {}
+
+  private void addCreative(CreativeModeTabEvent.BuildContents event) {}
 
   @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
   public static class ClientModEvents {
