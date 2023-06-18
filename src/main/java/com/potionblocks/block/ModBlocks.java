ecae8003e4123a03143e52246be4e45658db1d93
omerbenda <omerbenda@gmail.com>
2023-06-18 19:31:54 +0300

    feat(ModBlocks.java, empty_potion_block): Add the empty potion block

--- src/main/java/com/potionblocks/block/ModBlocks.java
+++ src/main/java/com/potionblocks/block/ModBlocks.java
@@ -7,6 +7,8 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,6 +19,15 @@ import java.util.function.Supplier;
 public class ModBlocks {
   public static final DeferredRegister<Block> BLOCKS =
       DeferredRegister.create(ForgeRegistries.BLOCKS, PotionBlocks.MOD_ID);
+
+  public static final RegistryObject<Block> EMPTY_POTION_BLOCK =
+      registerBlock(
+          "empty_potion_block",
+          () ->
+              new Block(
+                  BlockBehaviour.Properties.of(Material.METAL)
+                      .strength(6f)
+                      .requiresCorrectToolForDrops()));
 
   public static final RegistryObject<PotionBlock> SPEED_POTION_BLOCK =
       registerBlock(
