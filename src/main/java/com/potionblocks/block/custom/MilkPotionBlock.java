5a16e5330f52dc812fd423ea837a635ded294f59
omerbenda <omerbenda@gmail.com>
2023-06-22 18:49:54 +0300

    feat(MilkPotionBlock.java): Change of the empty potion block to a normal block and add the milk potion block

--- src/main/java/com/potionblocks/block/custom/MilkPotionBlock.java
+++ src/main/java/com/potionblocks/block/custom/MilkPotionBlock.java
@@ -9,9 +9,9 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 
-public class EmptyPotionBlock extends Block {
+public class MilkPotionBlock extends Block {
 
-  public EmptyPotionBlock() {
+  public MilkPotionBlock() {
     super(BlockBehaviour.Properties.of(Material.METAL).strength(6f).requiresCorrectToolForDrops());
   }
 
